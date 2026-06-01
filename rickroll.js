c819b5d71fb792befd1bec8a7c96af94afc8a08a
testingcodin <teo1202005@gmail.com>
2026-06-01 15:55:34 -0500

    Add files via upload

--- rickroll.js
+++ rickroll.js
@@ -3,7 +3,7 @@ function rickroll() {
     document.getElementById('content').innerHTML = 'get rickrolled';
     var rickroll =  document.getElementById('rickroll');
     rickroll.hidden = false;
-    var iframe = document.getElementById('iframe').innerHTML;
+    var iframe = document.getElementById('iframe')
     var player = new Vimeo.Player(iframe);
     player.play()
     player.setMuted(false)
